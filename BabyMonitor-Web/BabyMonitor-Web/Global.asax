a71beb96cde633086a1c35439cfbdb436a1616ff
Rami Sarieddine <r.sarieddine@live.com>
2016-04-21 14:43:37 +0300

    upload all

---
+++ BabyMonitor-Web/BabyMonitor-Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BabyMonitor_Web.MvcApplication" Language="C#" %>
---
+++ BabyMonitor-Web/BabyMonitor-Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BabyMonitor_Web.Startup))]
+namespace BabyMonitor_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
