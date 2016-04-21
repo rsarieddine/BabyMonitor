0a579834695a1df5b5d6cb7d568d83e507959ea4
drussell <drussell@microsoft.com>
2016-04-21 14:10:53 +0200

    Added UWP

---
+++ BabyMonitor-Web/BabyMonitorApp/Helpers/BaseCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BabyMonitorApp.Helpers
+{
+    public class BaseCommand : ICommand
+    {
+        private Action _task;
+        public BaseCommand(Action task)
+        {
+            _task = task;
+            active = true;
+
+        }
+        public bool CanExecute(object parameter) => active;
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            if(null != _task)_task();
+        }
+
+        private bool active;
+
+        public bool Active
+        {
+            get { return active; }
+            set { active = value; if(null!= CanExecuteChanged) CanExecuteChanged(this, new EventArgs()); }
+        }
+    }
+}
