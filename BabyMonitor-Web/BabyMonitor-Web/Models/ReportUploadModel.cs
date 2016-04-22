22e0f885ead8d0452109181034e9e7cbd39a0544
Rami Sarieddine <r.sarieddine@live.com>
2016-04-22 09:03:32 +0300

    added report upload functionality

---
+++ BabyMonitor-Web/BabyMonitor-Web/Models/ReportUploadModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+
+namespace BabyMonitor_Web.Models
+{
+    public class ReportUploadModel
+    {
+        public string ReportName { get; set; }
+        public HttpPostedFileBase PbixReport { get; set; }
+        public string ReportCategory { get; set; }
+        public string NewReportCategory { get; set; }
+    }
+}
---
+++ BabyMonitor-Web/BabyMonitor-Web/Models/ReportViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.PowerBI.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabyMonitor_Web.Models
+{
+   public class ReportViewModel
+    {
+        public IReport Report { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
